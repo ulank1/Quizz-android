dde9beeb63021a6d9ebd57616fc66880b58e072f
ulank1 <ulankarimov@mail.ru>
2020-05-06 01:42:04 +0600

    sdsd

---
+++ app/src/main/java/kg/mvvmdordoi/ui/ort/ResultActivity.kt
@@ -0,0 +1,16 @@
+package kg.mvvmdordoi.ui.ort
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import kg.mvvmdordoi.R
+
+class ResultActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_result)
+
+
+
+    }
+}
